2475d0edfcf1798cd677941e53efb715d603079c
Pubodee Kamonmasmatakul <Grit@trienpont.com>
2023-08-03 13:50:31 +0700

    add code comment

--- seo-general-test-template/back-end/compare-favicon/compare-favicon.ts
+++ seo-general-test-template/back-end/compare-favicon/compare-favicon.ts
@@ -6,13 +6,17 @@ import Jimp from "jimp";
 export const revalidate = 0;
 
 export default async function CompareFavicon(data: project_name_join_all) {
+  //get supahase record
   const base64Favicon = data.favicons.pop();
   const projectUrl = data.urls.pop();
+
+  // get supabase favicon base64 buffer
   const base64buffer = base64Favicon
     ? Buffer.from(base64Favicon?.favicons, "base64")
     : undefined;
-  const faviconData: ArrayBuffer = await FetchFavicon(projectUrl?.url);
 
+  // get site buffer
+  const faviconData: ArrayBuffer = await FetchFavicon(projectUrl?.url);
   const pngBuffer = ico
     .parse(faviconData, "image/png")
     .then((images) => Buffer.from(images[0].buffer));
@@ -21,11 +25,14 @@ export default async function CompareFavicon(data: project_name_join_all) {
     .then(([base64Buffer, pngBuffer]) => {
       if (!base64Buffer) return false;
       else {
+        // read both images
         const base64BufferRead = Jimp.read(base64Buffer);
         const pngBufferRead = Jimp.read(pngBuffer);
         return Promise.all([base64BufferRead, pngBufferRead]).then(
           ([base64Jimp, pngJimp]) => {
+            // compare with pics distance
             const picsDistance = Jimp.distance(base64Jimp, pngJimp);
+            // compare with pics diff
             const picsDiff = Jimp.diff(base64Jimp, pngJimp);
             console.log(picsDistance, "picsDistance");
             console.log(picsDiff.percent, "picsDiff");
